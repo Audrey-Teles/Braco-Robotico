589a75021ac9ba2fb7f1a381203970f32f7100b6
Audrey-Teles <audreytelesds@gmail.com>
2022-02-12 15:45:56 -0300

    Código para o Arduino

---
+++ Braco_Robotico/braco_com_python.ino
@@ -0,0 +1,16 @@
+#include <Servo.h> 
+
+Servo servo;
+
+void setup() {
+  Serial.begin(9600); 
+  servo.attach(6);
+}
+
+void loop() {
+  while (!Serial.available()){
+    int x = Serial.readString().toInt();
+    servo.write(x);
+    Serial.println(x);    
+  }
+}
